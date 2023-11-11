bfeb6aaa3016471937bc9e441651d4b490046e3c
prajaktapatil-18 <prajupatil0327@gmail.com>
2023-11-11 17:48:59 -0500

    total character of string

---
+++ src/Array/TotalCharacters.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.Scanner;
+
+public class TotalCharacters {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        String num = scan.nextLine();
+//
+
+//        String name = "prajakta";
+
+        for (int i =1;i<=num.length();i++){
+
+        int total = num.length();
+
+            System.out.println(total);
+            break;
+
+        }
+
+
+
+    }
+
+
+}
