b2278737123fb94c5bc90b0453ec9e138251c6d1
prajaktapatil-18 <prajupatil0327@gmail.com>
2023-11-13 12:12:01 -0500

    elements of an array present on even position

---
+++ src/Array/EvenPosition.java
@@ -0,0 +1,16 @@
+package Array;
+
+import java.util.Scanner;
+
+public class EvenPosition {
+    public static void main(String [] args){
+
+
+        int arr[] ={1,3,5,6,9,5,4,677,53,34,566,2,2,3,11,22,44,56};
+
+        for (int i =0;i<arr.length;i=i+2){
+            System.out.println(arr[i]);
+
+        }
+    }
+}
