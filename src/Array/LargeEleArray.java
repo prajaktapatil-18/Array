3c5788247b46e68028fc4a33c0780e2e9a04fd6e
prajaktapatil-18 <prajupatil0327@gmail.com>
2023-11-16 22:11:54 -0500

    largest element in an array

---
+++ src/Array/LargeEleArray.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class LargeEleArray {
+    public static void main(String[] args) {
+
+        int arr[] = {3,55,434,766,423,77,444,398};
+int max = 0;
+
+
+    for (int i =0;i<arr.length;i++){
+        if (arr[i]>max){
+            max = arr[i];
+
+        }
+
+        }
+        System.out.println(max);
+
+    }
+}
